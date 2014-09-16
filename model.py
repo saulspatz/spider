f54b463b318205dd31efe6d0772fe35e70ffd2f2
Saul Spatz <saul.spatz@gmail.com>
2014-09-15 20:19:10 -0500

    Initial commit

---
+++ model.py
@@ -0,0 +1,438 @@
+# model.py Model for spider solitaire
+
+import random, itertools, pickle
+
+ACE = 1
+JACK = 11
+QUEEN = 12
+KING = 13
+ALLRANKS = range(1, 14)      # one more than the highest value
+
+# RANKNAMES is a list that maps a rank to a string.  It contains a
+# dummy element at index 0 so it can be indexed directly with the card
+# value.
+
+SUITNAMES = ('club', 'diamond', 'heart', 'spade')
+RANKNAMES = ["", "Ace"] + list(map(str, range(2, 11))) + ["Jack", "Queen", "King"]
+COLORNAMES = ("red", "blue")     # back colors
+
+DEAL = (0, 0, 10)     # used in undo/redo stacks
+
+class Stack(list):
+  '''
+  A pile of cards.
+  The base class deals with the essential facilities of a stack, and the derived 
+  classes deal with presentation.
+  
+  The stack knows what cards it contains, but the card does not know which stack it is in.
+  
+  In reading the code you should realize that > and < for cards indicate successor and
+  predecessor, so that Ace of Hearts < Two of Hearts, but no other card.
+  '''
+  def __init__(self):
+    # Bottom card is self[0]; top is self[-1]
+    super().__init__()
+        
+  def add(self, card, faceUp = True):
+    self.append(card)
+    if faceUp:
+      self[-1].showFace()
+        
+  def isEmpty(self):
+    return not self
+    
+  def clear(self):
+    self[:] = []  
+      
+  def find(self, code):
+    '''
+    If the card with the given stack is in the stack,
+    return its index.  If not, return -1.
+    '''
+    for idx, card in enumerate(self):
+      if card.code == code:
+        return idx
+    return -1
+        
+class SelectableStack(Stack):
+  '''
+  A stack from which cards can be chosen, if they are face up and in sequence,
+  from the top of the stack.  When cards are removed, the top card is automatically
+  turned up, if it is not laready face up.
+  '''
+  def __init__(self):
+    super().__init__()
+    
+  def grab(self, n):
+    '''
+    Remove the card at index k and all those on top of it.
+    '''    
+    answer = self[k:]
+    self = self[:k]
+    return answer
+        
+  def replace(self, cards):
+    '''
+    Move aborted.  Replace these cards on the stack.
+    '''
+    self.extend(cards)
+    self.moving = None
+    
+  def canSelect(self, idx):
+    if idx >= len(self):
+      return False
+    if self[idx].faceDown():
+      return False
+    if not Card.isDescending(self[idx:]):
+      return False
+    return True
+      
+class OneWayStack(Stack):
+  '''
+  Used for the stock and the foundations.
+  No cards can be selected.
+  Cards are either all face up, or all face down.
+  '''
+  def __init__(self, faceUp):
+    super().__init__()
+    self.faceUp = faceUp
+    
+  def add(self, card):
+    super().add(card, self.faceUp)
+
+class Card:
+  '''
+  A card is identified by its rank, suit, and back color.
+  A card knows whether it is face up or own, but does not know 
+  which stack it is in.
+  '''
+  def __init__(self, rank, suit, back):
+    self.rank = rank
+    self.suit = suit
+    self.back = back
+    self.up = False   # all cards are initially face down
+    self.code = 52*COLORNAMES.index(back)+13*SUITNAMES.index(suit)+rank-1  
+
+  def showFace(self):
+    self.up = True
+    
+  def showBack(self):
+    self.up = False
+    
+  def faceUp(self):
+    return self.up
+  
+  def faceDown(self):
+    return not self.faceUp()
+  
+  # Overloaded operators for predecessor and successor
+  
+  def __lt__(self, other):
+    return self.suit == other.suit and self.rank == other.rank-1
+  
+  def __gt__(self, other):
+    return other < self
+  
+  def __repr__(self):
+    return '%s %s %s'%(self.suit, RANKNAMES[self.rank], self.back)
+  
+  def __str__(self):
+    return __repr__(self)
+  
+  @staticmethod
+  def isDescending(seq):
+    '''
+    Are the cards in a descending sequence of the same suit?
+    '''
+    return all(map(lambda x, y: x > y, seq, seq[1:]))  
+
+class Model:
+  '''
+  The cards are all in self.deck, and are copied into the appropriate stacks:
+      the stock
+      10 waste piles, where all the action is
+      8 foundation piles for completed suits
+  All entries on the undo and redo stacks are in the form (source, target, n), where
+      waste piles are numbered 0 to 9 and foundations 10 to 17, and n is the number
+      of cards moved, except that the entry (0, 0, 10) indicates dealing a row of cards,
+      and an entry of the form (source, source, 0) indicates turning the top card 
+      of the pile face up.
+      
+    '''
+  def __init__(self):
+    random.seed()
+    self.deck = []
+    self.selection = []
+    self.undoStack = []
+    self.redoStack = []
+    self.createCards()
+    self.stock = OneWayStack(False)
+    self.foundations = []
+    for k in range(8):
+      self.foundations.append(OneWayStack(True))
+    self.waste = []
+    for k in range(10):
+      self.waste.append(SelectableStack()) 
+    self.deal()
+    
+  def shuffle(self):
+    self.stock.clear()
+    for f in self.foundations:
+      f.clear()
+    for w in self.waste:
+      w.clear()
+    random.shuffle(self.deck)
+    for card in self.deck:
+      card.showBack()
+    self.stock.extend(self.deck)
+      
+  def createCards(self):
+    for rank, suit, back in itertools.product(ALLRANKS, SUITNAMES, COLORNAMES):
+      self.deck.append(Card(rank, suit, back))
+  
+  def deal(self):
+    self.shuffle()
+    self.dealDown()
+    self.dealUp()
+    self.undoStack = []
+    self.redoStack = []    
+    
+  def dealDown(self):
+    '''
+    Deal the face down cards into the initial layout.
+    '''
+    for n in range(44):
+      card = self.stock.pop()
+      self.waste[n%10].add(card, False)
+      
+  def dealUp(self, redo=False):
+    '''
+    Deal one row of face up cards
+    redo is True if we are redoing a deal
+    '''
+    for n in range(10):
+      card = self.stock.pop()
+      self.waste[n].add(card, True)
+    self.undoStack.append(DEAL)
+    if not redo:
+      self.redoStack = []
+      
+  def canDeal(self):
+    '''
+    Face up cards can be dealt only if no waste pile is empty
+    '''
+    return all(self.waste)
+         
+  def gameWon(self):
+    '''
+    The game is won when all foundation piles are used
+    '''
+    return min([len(f) for f in self.foundations]) == 13
+  
+  def downUp(self, k):
+    '''
+    Return a tuple (down, up) with the number of face down
+    cards and the number of face up cards in waste[k]
+    '''
+    w = self.waste[k]
+    down = len([card for card in w if card.faceDown()])
+    return (down, len(w) - down)
+  
+  def grab(self, k, idx):
+    '''
+    Initiate a move between waste 
+    Grab card idx and those on top of it from waste pile k
+    Return a code numbers of the selected cards.
+    We need to remember the data, since the move may fail.
+    '''
+    w = self.waste[k]
+    if not w.canSelect(idx):
+      return []
+    self.moveOrigin = k
+    self.moveIndex = idx
+    self.selection = w[idx:]
+    return self.selection
+  
+  def abortMove(self):
+    self.selection = []
+    
+  def moving(self):
+    return self.selection != [] 
+  
+  def getSelected(self):
+    return self.selection
+  
+  def canDrop(self, k):
+    '''
+    Can the moving cards be dropped on waste pile k?
+    '''
+    dest = self.waste[k]
+    source = self.selection
+    
+    if not self.selection:
+      return False
+    if not dest:
+      return True                                               # can always drop on empty pile
+    return dest[-1].rank - source[0].rank == 1  # e.g. can drop a 4 on a 5
+  
+  def completeMove(self, dest):
+    '''
+    Compete a legal move.
+    Tranfer the moving cards to the destination stack.
+    Turn the top card of the source stack face up, if need be.
+    '''
+    source = self.waste[self.moveOrigin]
+    moving = self.selection
+    target = self.waste[dest] if dest < 10 else self.foundations[dest-10]
+    target.extend(self.selection)
+    #while len(source) > self.moveIndex:
+      #source.pop()
+    source = source[:self.moveIndex]
+    self.undoStack.append((self.moveOrigin, dest, len(self.selection)))
+    self.flipTop(self.moveOrigin)
+    self.selection = []
+    self.redoStack = []
+    
+  def selectionToFoundation(self, dest):
+    '''
+    Complete a legal move to foundation pile
+    '''
+    self.completeMove(dest+10)
+    
+  def selectionToWaste(self, dest):
+    '''
+    Complete a legal move to waste pile
+    '''
+    self.completeMove(dest)  
+        
+  def completeSuit(self, pile, idx):
+    '''
+    *** Performs an action, and returns True (success) or False (failure).  ***
+    If pile contains a comple suit, and index indicates one of the 
+    top 13 cards, move the suit to the first available foundation,
+    and return True.  Otherwise, return False.
+    '''
+    w = self.waste[pile]
+    if len(w) < 13 or idx < len(w) - 13:
+      return False
+    
+    if w[-13].faceDown() :
+      return False
+    if not Card.isDescending(w[-13:]):
+        return False
+      
+    for i, f in enumerate(self.foundations):
+      if f.isEmpty(): break
+    for  card in w[-13:]:
+      f.add(card)
+    #for k in range(13):
+      #w.pop()
+    w = w[:-13]
+    self.flipTop(pile)
+    self.undoStack.append((pile, i+10, 13))
+    self.redoStack = []
+    return True
+  
+  def flipTop(self, k):
+    '''
+    Turn the top card of waste pile k face up, if need be
+    '''
+    w = self.waste[k]
+    try:
+      if w[-1].faceDown():
+        w[-1].showFace()
+        self.undoStack.append((k,k,0))
+    except IndexError:
+      pass
+  
+  def movingCompleteSuit(self):
+      return len(self.selection) == 13
+    
+  def win(self):
+    return all((len(f) for f in self.foundations)) 
+  
+  def undo(self):
+    ''''
+    Pop a record off the undo stack and undo the corresponding move.
+    If the move is a flipTop, undo the next move also.
+    '''
+    (s, t, n) = self.undoStack.pop()
+    if (s, t, n) == DEAL:
+      self.undeal()
+    elif s == t:      # flipTop called
+      self.waste[s][-1].showBack()
+      self.redoStack.append((s,t,n))
+      self.undo()
+    else:
+      source = self.waste[s] if s < 10 else self.foundations[s-10]
+      target = self.waste[t] if t < 10 else self.foundations[t-10]
+      assert len(target) >= n
+      source.extend(target[-n:])
+      #for k in range(n):
+        #target.pop()
+      target = target[:-n]
+      self.redoStack.append((s,t,n))
+  
+  def undeal(self):
+    '''
+    Undo a deal of a row of cards
+    '''
+    for w in reversed(self.waste):
+      assert w
+      card = w.pop()
+      card.showBack()
+      self.stock.append(card)
+    self.redoStack.append(DEAL)
+
+  def redo(self):
+    ''''
+    Pop a record off the redo stack and redo the corresponding move.
+    If the next move is a flipTop, redo the next move also.
+    ''' 
+    (s, t, n) = self.redoStack.pop()
+    if (s, t, n) == DEAL:
+      self.dealUp(True) 
+    else:
+      source = self.waste[s] if s < 10 else self.foundations[s-10]
+      target = self.waste[t] if t < 10 else self.foundations[t-10]
+      assert n <= len(source)
+      target.extend(source[-n:])
+      #for k in range(n):
+        #source.pop()
+      source = source[:-n]  
+    self.undoStack.append((s,t,n))
+    
+    try:
+      (s, t, n) = self.redoStack[-1]
+      if  s == t and n == 0:    # flip top
+        self.redoStack.pop()
+        self.waste[s][-1].showFace()
+        self.undoStack.append((s, t, n))
+    except IndexError:
+      pass
+      
+  def canUndo(self):
+    return self.undoStack != []
+  
+  def canRedo(self):
+      return self.redoStack != []  
+    
+  def save(self, filename):
+    with open(filename, 'wb') as fn:
+      pickle.dump((self.deck, self.undoStack, self.redoStack, self.stock, self.foundations, self.waste), fn)
+      
+  def load(self, filename):
+    '''
+    Read a saved game from filename, reconstitute the game, and display it.
+    '''
+    with open(filename, 'rb') as fin:
+      self.deck, self.undoStack, self.redoStack, self.stock, self.foundations, self.waste = pickle.load(fin) 
+    
+        
+    
+      
+      
+      
+      
+    
+                  
---
+++ view.py
@@ -0,0 +1,337 @@
+# view.py
+'''
+The visual interface for spider solitaire.
+The view knows about the model, but not vice versa
+Thce canvas widget is used for both view and controller.
+'''
+import sys, os, itertools
+import tkinter as tk
+from model import SUITNAMES, RANKNAMES, ALLRANKS, Card
+from tkinter.messagebox import showerror
+from utils import ScrolledCanvas
+
+# Constants determining the size and layout of cards and stacks.  We
+# work in a grid where each stack is surrounded by MARGIN
+# pixels of space on each side, so adjacent stacks are separated by
+# 2*MARGIN pixels.  OFFSET1 is the offset used for displaying the
+# a card above a face down card, and OFFSET2 is the offset used
+# for displaying a card above a face up card.
+
+CARDWIDTH = 71
+CARDHEIGHT = 96
+MARGIN = 10
+XSPACING = CARDWIDTH + 2*MARGIN
+YSPACING = CARDHEIGHT + 4*MARGIN
+OFFSET1 = 8
+OFFSET2 = 25
+
+BACKGROUND = '#070'
+OUTLINE = '#060'        # outline color of foundation files
+CELEBRATE = 'yellow'     # Color of "you won" message
+BUTTON = 'forest green'
+
+# Cursors
+DEFAULT_CURSOR = 'arrow'
+SELECT_CURSOR = 'hand2'
+
+imageDict = {}   # hang on to images, or they may disappear!
+
+class View: 
+  '''
+  Cards are represented as canvas image iitems,  displaying either the face
+  or the back as appropriate.  Each card has the tag "card".  This is 
+  crucial, since only canvas items tagged "card" will respond to mouse
+  clicks.
+  '''
+  def __init__(self, parent, width, height):
+    self.parent = parent          # parent is the Spider application
+    self.model =  parent.model
+    self.root = root = tk.Tk()
+    root.title("Spider Solitaire")
+    self.menu = tk.Menu(root)         # parent constructs actual menu         
+    root.config(menu=self.menu)                 
+    self.waste = []           # NW corners of the waste piles
+    self.foundations = []   # NW corners of the foundation piles
+    x = MARGIN
+    y = 5* MARGIN
+    self.stock = (x,y)      #NW corner of stock
+    x += XSPACING
+    for k in range(8):
+      x += XSPACING
+      self.foundations.append((x, y))
+    y += YSPACING
+    x = MARGIN
+    for k in range(10):
+      self.waste.append((x, y)) 
+      x += XSPACING 
+    tableau = self.tableau = ScrolledCanvas(root, width, height, BACKGROUND, DEFAULT_CURSOR, tk.VERTICAL)
+    tableau.pack(expand = tk.YES, fill=tk.BOTH) 
+    
+    self.undoButton = tableau.create_oval(width//2-4*MARGIN, MARGIN, width//2+2*MARGIN, 4*MARGIN, 
+                                          fill = BUTTON, outline = BUTTON, tag = "undo")
+    self.redoButton = tableau.create_oval(width//2+4*MARGIN, MARGIN, width//2+10*MARGIN, 4*MARGIN, 
+                                          fill = BUTTON, outline = BUTTON, tag = "redo")
+    tableau.create_text(width//2-MARGIN, 2.5*MARGIN, text='Undo', fill = CELEBRATE, tag = 'undo', anchor=tk.CENTER)
+    tableau.create_text(width//2+7*MARGIN, 2.5*MARGIN, text='Redo', fill = CELEBRATE, tag = 'redo', anchor=tk.CENTER)
+    self.loadImages()
+    self.createCards()
+    tableau.tag_bind("card", '<ButtonPress-1>', self.onClick)
+    tableau.tag_bind("card", '<Double-Button-1>', self.onDoubleClick)
+    tableau.canvas.bind('<B1-Motion>', self.drag)
+    tableau.canvas.bind('<ButtonRelease-1>', self.onDrop)
+    tableau.tag_bind('undo', '<ButtonPress-1>', self.undo)
+    tableau.tag_bind('redo', '<ButtonPress-1>', self.redo)
+    for f in self.foundations:
+      tableau.create_rectangle(f[0], f[1], f[0]+CARDWIDTH, f[1]+CARDHEIGHT, outline = OUTLINE)
+    tableau.create_text(self.foundations[0][0], self.foundations[0][1]+CARDHEIGHT, 
+                        text = "'The game is done! I've won! I've won!'\nQuoth she, and whistles thrice.",
+                        fill = BACKGROUND, font=("Times", "32", "bold"), tag = 'winText', anchor=tk.NW)
+    self.show()
+    
+  def start(self):
+    self.root.mainloop()
+      
+  def loadImages(self):
+    PhotoImage = tk.PhotoImage
+    cardDir = os.path.join(os.path.dirname(sys.argv[0]), 'cards') 
+    blue = PhotoImage(file=os.path.join(cardDir,'blueBackVert.gif'))
+    red = PhotoImage(file=os.path.join(cardDir,'redBackVert.gif'))
+    imageDict['blue'] = blue
+    imageDict['red'] = red    
+    for rank, suit in itertools.product(ALLRANKS, SUITNAMES):
+      face = PhotoImage(file = os.path.join(cardDir, suit+RANKNAMES[rank]+'.gif'))               
+      imageDict[rank, suit] = face
+      
+  def createCards(self):
+    model = self.model
+    canvas = self.tableau    
+    for card in model.deck:
+      c = canvas.create_image(-200, -200, image = None, anchor = tk.NW, tag = "card")
+      canvas.addtag_withtag('code%d'%card.code, c)
+      
+  def showWaste(self, k):
+    '''
+    Display waste pile number k
+    '''
+    x, y = self.waste[k]
+    canvas = self.tableau
+    for card in self.model.waste[k]:
+      tag = 'code%d'%card.code
+      canvas.coords(tag, x, y)
+      if card.faceUp():
+        foto = imageDict[card.rank, card.suit]
+        y += OFFSET2
+      else:
+        foto = imageDict[card.back]
+        y += OFFSET1
+      canvas.itemconfigure(tag, image = foto)
+      canvas.tag_raise(tag) 
+      
+  def show(self):
+    model = self.model
+    canvas = self.tableau
+    self.showStock()
+    for k in range(10):
+      self.showWaste(k)
+    for k in range(8):
+      self.showFoundation(k)
+    if model.canUndo():
+      self.enableUndo()
+    else:
+      self.disableUndo()
+    if model.canRedo():
+      self.enableRedo()
+    else:
+      self.disableRedo()   
+    color = CELEBRATE if model.win() else BACKGROUND
+    canvas.itemconfigure('winText', fill=color)
+    
+  def dealUp(self):
+    self.model.dealUp()
+    self.show()
+      
+  def showFoundation(self, k):
+    model = self.model
+    canvas = self.tableau
+    x, y = self.foundations[k]
+    for card in model.foundations[k]:
+      tag = 'code%d'%card.code
+      canvas.itemconfigure(tag, image = imageDict[card.rank, card.suit])
+      canvas.coords(tag,x,y)
+      canvas.tag_raise(tag)
+      
+  def showStock(self):
+    model = self.model
+    canvas = self.tableau
+    x, y = self.stock
+    for card in model.stock:
+      tag = 'code%d'%card.code
+      canvas.itemconfigure(tag, image = imageDict[card.back])
+      canvas.coords(tag,x,y)
+      canvas.tag_raise(tag)    
+                   
+  def grab(self, selection, k, mouseX, mouseY):
+    '''
+    Grab the cards in selection.
+    k is the index of the source waste pile.
+    Note that all the cards are face up.
+    '''
+    canvas = self.tableau
+    if not selection:
+      return
+    self.mouseX, self.mouseY = mouseX, mouseY
+    west = self.waste[k][0]
+    for card in selection:
+      tag = 'code%s'%card.code
+      canvas.tag_raise(tag)
+      canvas.addtag_withtag("floating", tag)
+    canvas.configure(cursor=SELECT_CURSOR)
+    dx = 5 if mouseX - west > 10 else -5
+    canvas.move('floating', dx, 0)
+    
+  def drag(self, event):
+    try:
+      x, y = event.x, event.y
+      dx, dy = x - self.mouseX, y - self.mouseY
+      self.mouseX, self.mouseY = x, y
+      self.tableau.move('floating', dx, dy)
+    except AttributeError:
+      pass
+  
+  def onClick(self, event):
+    '''
+    Respond to click on stock or waste pile.  
+    Clicks on foundation piles are ignored.
+    '''
+    model = self.model
+    canvas = self.tableau
+    tag = [t for t in canvas.gettags('current') if t.startswith('code')][0]
+    code = int(tag[4:])             # code of the card clicked
+    if model.stock.find(code) != -1:
+      if model.canDeal():
+        self.dealUp()
+        return
+      else:
+        self.cannotDeal()
+    for k, w in enumerate(model.waste):
+      idx = w.find(code)
+      if idx != -1:
+        break
+    else:       # loop else
+      return
+    selection = model.grab(k, idx)
+    self.grab(selection, k, event.x, event.y)  
+    
+  def onDoubleClick(self, event):
+    '''
+    If the user double clicks a card that is part of a complete suit,
+    the suit will be move to the first available foundation pile.
+    '''
+    model = self.model
+    canvas = self.tableau
+    tag = [t for t in canvas.gettags('current') if t.startswith('code')][0]
+    code = int(tag[4:])             # code of the card clicked
+    for k, w in enumerate(model.waste):
+      idx = w.find(code)
+      if idx != -1:
+        break
+    else:       # loop else
+      return 
+    model.completeSuit(k, idx)
+    self.show()
+   
+  def onDrop(self, event):
+    '''
+    Drop the selected cards.  In order to recognize the destination waste pile,
+    the y value of the cursor position must be below the top of the waste piles,
+    and either the left edge of the floating cards must be in the left half of the
+    pile, or the right edge must be in the right half.
+    
+    If the selection being dragged is a complete suit, the destination must be a
+    foundation pile.
+    '''
+    model = self.model
+    if not model.moving():
+      return
+    canvas = self.tableau
+    canvas.configure(cursor=DEFAULT_CURSOR)
+    
+    f = canvas.find_withtag('floating')[0]
+    left = canvas.coords(f)[0]
+    right = left + CARDWIDTH
+    
+    def findDestInArray(seq):
+      if event.y < seq[0][1]:
+        return -1      
+      for k, w in enumerate(seq):
+        west = w[0]       
+        center = west + CARDWIDTH/2
+        east = west + CARDWIDTH
+        if (west <= left <= center) or (center <= right <= east):
+          return  k
+      return -1
+  
+    pile = findDestInArray(self.waste)
+    
+    if pile != -1:     # dropped on waste pile
+      if pile == model.moveOrigin:
+        self.abortMove()                      # dropped on source pile
+      elif not model.canDrop(pile):  
+        self.abortMove()                     # invalid destination
+      else:
+        self.completeMove(pile)
+    elif model.movingCompleteSuit():
+      #check for drop on foundation pile
+      pile = findDestInArray(self.foundations)
+      if pile != -1 and model.foundations[pile].isEmpty():
+        self.suitToFoundation(pile)
+      else:
+        self.abortMove()
+    else:
+      self.abortMove()
+      
+    self.show()
+       
+  def abortMove(self):
+    self.model.abortMove()
+    self.showWaste(self.model.moveOrigin)
+    self.tableau.dtag('floating', 'floating')
+         
+  def completeMove(self, dest):
+    model = self.model
+    source = model.moveOrigin
+    model.selectionToWaste(dest)
+    self.show()
+    self.tableau.dtag('floating', 'floating')
+    
+  def suitToFoundation(self, dest):
+    model = self.model
+    source = model.moveOrigin
+    model.selectionToFoundation(dest)
+    self.show()
+    self.tableau.dtag('floating', 'floating')    
+  
+  def cannotDeal(self):
+    showerror('Cannot deal', "Can't deal with empty pile.")
+    
+  def undo(self, event):
+    self.model.undo()
+    self.show()
+    
+  def redo(self, event):
+    self.model.redo()
+    self.show()  
+    
+  def disableRedo(self):
+    self.tableau.itemconfigure('redo', state=tk.HIDDEN)
+  
+  def disableUndo(self):
+    self.tableau.itemconfigure('undo', state=tk.HIDDEN)
+  
+
+  def enableRedo(self):
+    self.tableau.itemconfigure('redo', state=tk.NORMAL)
+  
+    
+  def enableUndo(self):
+    self.tableau.itemconfigure('undo', state=tk.NORMAL)
